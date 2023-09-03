6dcd7390849435e63731ad619cfe4e50a43e56dd
dhanyarajagopalan <dhanyagan2009@gmail.com>
2023-09-03 14:34:05 -0400

    hyperparameter training

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -6,6 +6,10 @@ import pandas as pd
 
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
+from src.components.data_transformation import DataTransformation
+from src.components.data_transformation import DataTransformationConfig
+from src.components.model_trainer import ModelTrainerConfig
+from src.components.model_trainer import ModelTrainer
 
 class DataIngestionConfig:
     train_data_path: str = os.path.join('artifacts',"train.csv")
@@ -31,6 +35,8 @@ class DataIngestion:
             
             train_set,test_set=train_test_split(df,test_size=0.2,random_state=42)
             
+            train_set.to_csv(self.ingestion_config.train_data_path,index=False,header=True)
+            
             test_set.to_csv(self.ingestion_config.test_data_path,index=False,header=True)
             
             logging.info("Ingestion of the data is completed")
@@ -45,4 +51,12 @@ class DataIngestion:
         
 if __name__ == "__main__":
      obj=DataIngestion()
-     obj.initiate_data_ingestion()                      
+     train_data,test_data=obj.initiate_data_ingestion() 
+     
+     logging.info("Start Transformation")
+     data_transformation=DataTransformation()
+     logging.info("assign Transformation")
+     train_arr,test_arr=data_transformation.initiate_data_transformation(train_data,test_data)
+     modeltrainer=ModelTrainer()
+     print(modeltrainer.initiate_model_trainer(train_arr,test_arr))  
+     
